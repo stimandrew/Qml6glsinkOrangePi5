8871bfeb0bc88f446c228a56832b110a62a63b11
Andrew Berillo <stim_andrew@mail.ru>
2025-06-17 09:44:46 +0300

    Update install_packages_target.sh

--- install_packages_target.sh
+++ install_packages_target.sh
@@ -117,7 +117,6 @@ sudo apt-get install -y libgstreamer1.0-dev
 sudo apt-get install -y libgstreamer-plugins-base1.0-dev
 sudo apt-get install -y libgstreamer-plugins-bad1.0-dev
 sudo apt-get install -y gstreamer1.0-plugins-base
-sudo apt-get install -y gstreamer1.0-plugins-good
 sudo apt-get install -y gstreamer1.0-plugins-bad
 sudo apt-get install -y gstreamer1.0-plugins-ugly
 sudo apt-get install -y gstreamer1.0-libav
@@ -126,9 +125,6 @@ sudo apt-get install -y gstreamer1.0-omx
 sudo apt-get install -y gstreamer1.0-x
 sudo apt-get install -y gstreamer1.0-alsa
 sudo apt-get install -y gstreamer1.0-gl
-sudo apt-get install -y gstreamer1.0-gtk3
-sudo apt-get install -y gstreamer1.0-qt5
-sudo apt-get install -y gstreamer1.0-qt6
 sudo apt-get install -y gstreamer1.0-pulseaudio
 sudo apt-get install -y libpulse-dev
 sudo apt-get install -y libalsa-ocaml-dev
